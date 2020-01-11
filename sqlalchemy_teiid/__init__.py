0383c57d594a6a04b7db3d66059b11c594151ffd
Kolmar Kafran <kolmar@kafran.net>
2020-01-10 21:55:15 -0300

    Working on table reflection

--- sqlalchemy_teiid/__init__.py
+++ sqlalchemy_teiid/__init__.py
@@ -17,13 +17,66 @@ import json
 import logging
 import re
 
+from sqlalchemy import sql, util
 from sqlalchemy.dialects.postgresql.psycopg2 import (
     PGCompiler_psycopg2,
     PGDialect_psycopg2,
 )
-from sqlalchemy.sql import elements
+from sqlalchemy.engine import reflection
+from sqlalchemy.sql import elements, sqltypes
+
+from sqlalchemy.types import BIGINT
+from sqlalchemy.types import BOOLEAN
+from sqlalchemy.types import CHAR
+from sqlalchemy.types import DATE
+from sqlalchemy.types import FLOAT
+from sqlalchemy.types import INTEGER
+from sqlalchemy.types import NUMERIC
+from sqlalchemy.types import REAL
+from sqlalchemy.types import SMALLINT
+from sqlalchemy.types import TEXT
+from sqlalchemy.types import VARCHAR
 
 logger = logging.getLogger("teiid")
+
+
+class TIMESTAMP(sqltypes.TIMESTAMP):
+    def __init__(self, timezone=False, precision=None):
+        super(TIMESTAMP, self).__init__(timezone=timezone)
+        self.precision = precision
+
+
+class TIME(sqltypes.TIME):
+    def __init__(self, timezone=False, precision=None):
+        super(TIME, self).__init__(timezone=timezone)
+        self.precision = precision
+
+
+ischema_names = {
+    "integer": INTEGER,
+    "bigint": BIGINT,
+    "long": BIGINT,  # TEIID
+    "smallint": SMALLINT,
+    "short": SMALLINT,  # TEIID
+    "character varying": VARCHAR,
+    "string": VARCHAR,  # TEIID
+    "character": CHAR,
+    '"char"': sqltypes.String,
+    "name": sqltypes.String,
+    "text": TEXT,
+    "numeric": NUMERIC,
+    "bigdecimal": NUMERIC,  # TEIID
+    "float": FLOAT,
+    "real": REAL,
+    "timestamp": TIMESTAMP,
+    "timestamp with time zone": TIMESTAMP,
+    "timestamp without time zone": TIMESTAMP,
+    "time with time zone": TIME,
+    "time without time zone": TIME,
+    "date": DATE,
+    "time": TIME,
+    "boolean": BOOLEAN,
+}
 
 
 class TeiidCompiler(PGCompiler_psycopg2):
@@ -90,5 +143,191 @@ class TeiidDialect(PGDialect_psycopg2):
         """
         return (8, 2)
 
+    @reflection.cache
+    def get_columns(self, connection, table_name, schema=None, **kw):
+
+        # table_oid = self.get_table_oid(
+        #     connection, table_name, schema, info_cache=kw.get("info_cache")
+        # )
+        SQL_COLS = """
+            SELECT Name,
+            DataType,
+            Scale,
+            Length,
+            CASE
+                WHEN NullType = 'No Nulls' THEN False
+                ELSE True
+            END AS "Nullable",
+            Position,
+            Description
+            FROM SYS.Columns
+            WHERE SchemaName = :schema AND TableName = :table_name
+            ORDER BY Position
+        """
+        s = (
+            sql.text(SQL_COLS)
+            .bindparams(
+                sql.bindparam("schema", type_=sqltypes.Unicode),
+                sql.bindparam("table_name", type_=sqltypes.Unicode),
+            )
+            .columns(Name=sqltypes.Unicode, Description=sqltypes.Unicode)
+        )
+        c = connection.execute(s, schema=schema, table_name=table_name)
+        rows = c.fetchall()
+
+        # dictionary with (name, ) if default search path or (schema, name)
+        # as keys
+        # domains = self._load_domains(connection)
+
+        # dictionary with (name, ) if default search path or (schema, name)
+        # as keys
+        # enums = dict(
+        #     ((rec["name"],), rec)
+        #     if rec["visible"]
+        #     else ((rec["schema"], rec["name"]), rec)
+        #     for rec in self._load_enums(connection, schema="*")
+        # )
+
+        # format columns
+        columns = []
+
+        for (name, datatype, scale, length, nullable, position, description) in rows:
+            column_info = self._get_column_info(
+                name, datatype, scale, length, nullable, schema, description
+            )
+            columns.append(column_info)
+        return columns
+
+    def _get_column_info(
+        self, name, datatype, scale, length, nullable, schema, description
+    ):
+        def _handle_array_type(attype):
+            return (
+                # strip '[]' from integer[], etc.
+                re.sub(r"\[\]$", "", attype),
+                attype.endswith("[]"),
+            )
+
+        # strip (*) from character varying(5), timestamp(5)
+        # with time zone, geometry(POLYGON), etc.
+        attype = re.sub(r"\(.*\)", "", datatype)
+
+        # strip '[]' from integer[], etc. and check if an array
+        attype, is_array = _handle_array_type(attype)
+
+        # strip quotes from case sensitive enum or domain names
+        # enum_or_domain_key = tuple(util.quoted_token_parser(attype))
+
+        # nullable = not notnull
+
+        charlen = re.search(r"\(([\d,]+)\)", datatype)
+        if charlen:
+            charlen = charlen.group(1)
+        args = re.search(r"\((.*)\)", datatype)
+        if args and args.group(1):
+            args = tuple(re.split(r"\s*,\s*", args.group(1)))
+        else:
+            args = ()
+        kwargs = {}
+
+        if attype == "numeric":
+            if charlen:
+                prec, scale = charlen.split(",")
+                args = (int(prec), int(scale))
+            else:
+                args = ()
+        elif attype == "double precision":
+            args = (53,)
+        elif attype == "integer":
+            args = ()
+        elif attype in ("timestamp with time zone", "time with time zone"):
+            kwargs["timezone"] = True
+            if charlen:
+                kwargs["precision"] = int(charlen)
+            args = ()
+        elif attype in (
+            "timestamp without time zone",
+            "time without time zone",
+            "time",
+        ):
+            kwargs["timezone"] = False
+            if charlen:
+                kwargs["precision"] = int(charlen)
+            args = ()
+        elif attype == "bit varying":
+            kwargs["varying"] = True
+            if charlen:
+                args = (int(charlen),)
+            else:
+                args = ()
+        elif attype.startswith("interval"):
+            field_match = re.match(r"interval (.+)", attype, re.I)
+            if charlen:
+                kwargs["precision"] = int(charlen)
+            if field_match:
+                kwargs["fields"] = field_match.group(1)
+            attype = "interval"
+            args = ()
+        elif charlen:
+            args = (int(charlen),)
+
+        while True:
+            # looping here to suit nested domains
+            if attype in self.ischema_names:
+                coltype = self.ischema_names[attype]
+                break
+            else:
+                coltype = None
+                break
+
+        if coltype:
+            coltype = coltype(*args, **kwargs)
+            if is_array:
+                coltype = self.ischema_names["_array"](coltype)
+        else:
+            util.warn("Did not recognize type '%s' of column '%s'" % (attype, name))
+            coltype = sqltypes.NULLTYPE
+
+        column_info = dict(
+            name=name,
+            type=coltype,
+            nullable=nullable,
+            default=None,
+            autoincrement=False,
+            comment=description,
+        )
+        return column_info
+
+    def get_pk_constraint(self, connection, table_name, schema=None, **kw):
+        pass
+
+    def get_foreign_keys(
+        self,
+        connection,
+        table_name,
+        schema=None,
+        postgresql_ignore_search_path=False,
+        **kw
+    ):
+        return [
+            dict(
+                name="",
+                constrained_columns=[],
+                referred_schema=schema,
+                referred_table=table_name,
+                referred_columns=[],
+            )
+        ]
+
+    def get_indexes(self, connection, table_name, schema, **kw):
+        return [dict(name="", column_names=[], unique=None)]
+
+    def get_unique_constraints(
+        self, connection, table_name, schema=None, **kw
+    ):
+        return [dict(name="", column_names=[])]
+
+    def get_check_constraints(self, connection, table_name, schema=None, **kw):
+        return [dict(name="", sqltext="", )]
 
 dialect = TeiidDialect
